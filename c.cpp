534219dc900888d289be231e59c522fd7ad4c6a5
Luv_Victory <973141796@qq.com>
2022-12-29 16:50:05 +0800

    add a.cpp b.cpp c.cpp

---
+++ c.cpp
@@ -0,0 +1,12 @@
+#include <iostream>  //cout, cin
+
+using namespace std;
+
+int main(){
+    cout << "hello world!" << endl;
+    cout << "hello world again!" << endl;
+    cout << "there is sth need to drawback" << endl;
+    system("pause");
+    return 0;
+}
+
